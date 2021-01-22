4598f784b9a34d3039980c5fe59ae7d477430e67
Henok Tilahun <henok.299312@nv.ccsd.net>
2021-01-22 20:34:14 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    Person p1 = new Person("Luke", "brony2077@gmail.com","555-123-4567");
+
+		Person p2 = new Person("Joseph", "xxAOTFanxx@gmail.com","555-987-6543");
+    
+    System.out.println(p1);
+  }
+}
---
+++ Person.java
@@ -0,0 +1,57 @@
+public class Person
+{
+   private String name;
+   private String email;
+   private String phoneNum;
+
+   public Person()
+	 {
+		 	name = "";
+			email = "";
+			phoneNum = "";
+	 }
+
+	 public Person(String initName, String initEmail, String initPhoneNum)
+   {
+      name = initName;
+			email = initEmail;
+			phoneNum = initPhoneNum;
+   }
+
+    public String getName()
+   {
+      return name;
+   }
+
+    public String getEmail()
+   {
+      return email;
+   }
+
+    public String getPhoneNum()
+   {
+      return phoneNum;
+   }
+
+    public void setName(String newName)
+   {
+      name = newName;
+   }
+
+    public void setEmail(String newEmail)
+   {
+      email = newEmail; 
+      
+    }
+
+    public void setPhoneNum(String newPhoneNum)
+    {
+      phoneNum = newPhoneNum;
+    }
+
+    public String toString()
+      {
+        return name + " : " + email + " : " + phoneNum;
+      }
+
+}
